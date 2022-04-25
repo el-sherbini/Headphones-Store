1ff598b6dbc06a04e45f6255cd4df57432156c20
Mohamed Emad <mohamed.elsherbiny215@gmail.com>
2022-04-25 12:08:22 +0200

    add Product component

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -2,25 +2,31 @@ import Link from "next/link";
 
 import { urlFor } from "../lib/client";
 
-const HeroBanner = ({ heroBanner }) => {
+const HeroBanner = ({
+  heroBanner: {
+    smallText,
+    midText,
+    largeText1,
+    image,
+    product,
+    buttonText,
+    desc,
+  },
+}) => {
   return (
     <div className="hero-banner-container">
-      <p className="beats-solo">{heroBanner.smallText}</p>
-      <h3>{heroBanner.midText}</h3>
-      <h1>{heroBanner.largeText1}</h1>
-      <img
-        src={urlFor(heroBanner.image)}
-        alt="headphones"
-        className="hero-banner-image"
-      />
+      <p className="beats-solo">{smallText}</p>
+      <h3>{midText}</h3>
+      <h1>{largeText1}</h1>
+      <img src={urlFor(image)} alt="headphones" className="hero-banner-image" />
 
       <div>
-        <Link href={`/product/${heroBanner.product}`} passHref>
-          <button type="button">{heroBanner.buttonText}</button>
+        <Link href={`/product/${product}`} passHref>
+          <button type="button">{buttonText}</button>
         </Link>
         <div className="desc">
           <h5>Description</h5>
-          <p>{heroBanner.desc}</p>
+          <p>{desc}</p>
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,17 @@ const Home = ({ products, bannerData }) => {
   return (
     <div>
       <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      <div className="products-heading">
+        <h2>Best Seller Products</h2>
+        <p>speaker There are many variations passages</p>
+      </div>
+
       <div className="products-container">
         {products?.map((product) => (
           <Product key={product._id} product={product} />
         ))}
       </div>
+
       <FooterBanner />
     </div>
   );
