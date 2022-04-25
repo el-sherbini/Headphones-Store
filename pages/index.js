a30452469c7f3556ea9ab8e27bbf582fd8fb88fd
Mohamed Emad <mohamed.elsherbiny215@gmail.com>
2022-04-25 11:42:05 +0200

    add HeroBanner component

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,30 @@
+import { client } from "../lib/client";
 import { Product, FooterBanner, HeroBanner } from "../components";
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <div>
-      <HeroBanner />
-      Products
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      <div className="products-container">
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
       <FooterBanner />
     </div>
   );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default Home;
